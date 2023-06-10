ce2a210d7db1d99b153e3b7936d31176c1f73633
samimahlaka <samimahlaka@gmail.com>
2023-06-09 21:10:17 -0500

    Define model for Todo app

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+class Todo (models.Model):
+    title = models.CharField(max_length=60)
+    discription = models.TextField()
+    completed = models.BooleanField(default='false')
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
 
 # Create your models here.
